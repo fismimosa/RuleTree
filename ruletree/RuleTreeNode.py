cc8b4a31c8fda0b20d69f9d35d1596a903c2bd61
acascione <117826770+acascione@users.noreply.github.com>
2024-08-13 18:51:38 +0200

    adding encode_node method

--- ruletree/RuleTreeNode.py
+++ ruletree/RuleTreeNode.py
@@ -81,3 +81,26 @@ class RuleTreeNode:
                 "left_node": self.node_l.get_rule(),
                 "right_node": self.node_r.get_rule(),
             }
+            
+    def encode_node(self, index, parent, vector, clf, node_index=0):
+        if self.is_leaf():
+            vector[0][node_index] = -1
+            vector[1][node_index] = self.prediction
+        else:            
+            feat = self.clf.feature_original[0]
+            thr = self.clf.threshold_original[0]
+         
+            vector[0][node_index] = feat + 1
+            vector[1][node_index] = thr
+
+            node_l = self.node_l
+            node_r = self.node_r
+
+            index[node_l.node_id] = 2 * node_index + 1
+            index[node_r.node_id] = 2 * node_index + 2
+            
+            parent[2*index[self.node_id] + 1] = index[self.node_id]
+            parent[2*index[self.node_id] + 2] = index[self.node_id]
+
+            node_l.encode_node(index, parent, vector, clf, 2 * node_index + 1)
+            node_r.encode_node(index, parent, vector, clf, 2 * node_index + 2)
