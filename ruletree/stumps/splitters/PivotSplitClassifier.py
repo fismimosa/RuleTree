789eff8828526fb9eb2d4469af69aaa6eeb07769
acascione <117826770+acascione@users.noreply.github.com>
2024-10-08 18:54:51 +0200

    added multiplesplitclf

--- ruletree/stumps/splitters/PivotSplitClassifier.py
+++ ruletree/stumps/splitters/PivotSplitClassifier.py
@@ -2,7 +2,13 @@ from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
 
 from ruletree.stumps.splitters.PivotSplit import PivotSplit
+from ruletree.stumps.splitters.PivotSplit import MultiplePivotSplit
+
 
 class PivotSplitClassifier(PivotSplit):
+    def get_base_model(self):
+        return DecisionTreeClassifier(**self.kwargs)
+    
+class MultiplePivotSplitClassifier(MultiplePivotSplit):
     def get_base_model(self):
         return DecisionTreeClassifier(**self.kwargs)
