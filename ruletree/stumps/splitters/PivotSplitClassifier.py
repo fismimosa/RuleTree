250f153f36f7377b4026fbb972874563178afec6
acascione <117826770+acascione@users.noreply.github.com>
2024-11-01 16:50:56 +0100

    added oblique_pivot_split

--- ruletree/stumps/splitters/PivotSplitClassifier.py
+++ ruletree/stumps/splitters/PivotSplitClassifier.py
@@ -1,8 +1,10 @@
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
 
-from ruletree.stumps.splitters.PivotSplit import PivotSplit
+from ruletree.stumps.splitters.PivotSplit import PivotSplit, ObliquePivotSplit
 from ruletree.stumps.splitters.PivotSplit import MultiplePivotSplit
+from ruletree.stumps.splitters.ObliqueBivariateSplitClassifier import ObliqueBivariateSplitClassifier
+from ruletree.stumps.splitters.ObliqueHouseHolderSplitClassifier import ObliqueHouseHolderSplitClassifier
 
 
 class PivotSplitClassifier(PivotSplit):
@@ -12,3 +14,14 @@ class PivotSplitClassifier(PivotSplit):
 class MultiplePivotSplitClassifier(MultiplePivotSplit):
     def get_base_model(self):
         return DecisionTreeClassifier(**self.kwargs)
+    
+class ObliquePivotSplitClassifier(ObliquePivotSplit):
+    def __init__(self, oblique_split_type = 'householder', **kwargs): 
+        super().__init__(**kwargs)  
+        self.oblique_split_type = oblique_split_type
+        
+    def get_base_model(self):
+        if self.oblique_split_type == 'householder':
+           return ObliqueHouseHolderSplitClassifier(**self.kwargs)
+        if self.oblique_split_type == 'bivariate':
+           return ObliqueBivariateSplitClassifier(**self.kwargs)
