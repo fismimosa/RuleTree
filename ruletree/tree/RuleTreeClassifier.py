c3b61840d3ca768c56584575a95c1bb0f0d7ee7b
acascione <117826770+acascione@users.noreply.github.com>
2024-12-08 01:00:52 +0100

    Update RuleTreeClassifier.py

--- ruletree/tree/RuleTreeClassifier.py
+++ ruletree/tree/RuleTreeClassifier.py
@@ -197,30 +197,6 @@ class RuleTreeClassifier(RuleTree, ClassifierMixin):
             self._get_prediction_probas(current_node.node_r, probas)
         
         return probas
-    
-    
-    def local_interpretation(self, X, joint_contribution = False):
-        leaves, paths, leaf_to_path, values = super().local_interpretation(X = X,
-                                                                           joint_contribution = joint_contribution)
-        
-        print(values)
-        # we require the values to be the same shape as the biases
-        values = values.squeeze(axis=1)
-        biases = np.full(X.shape[0], values[paths[0][0]])
-        line_shape = X.shape[1]
-
-        biases = np.tile(values[paths[0][0]], (X.shape[0], 1))
-        line_shape = (X.shape[1], self.n_classes_)
-        
-        return super().eval_contributions(
-                                        leaves=leaves,
-                                        paths=paths,
-                                        leaf_to_path=leaf_to_path,
-                                        values=values,
-                                        biases=biases,
-                                        line_shape=line_shape,
-                                        joint_contribution=joint_contribution
-                                    )
 
     def local_interpretation(self, X, joint_contribution = False):
         leaves, paths, leaf_to_path, values = super().local_interpretation(X = X,
