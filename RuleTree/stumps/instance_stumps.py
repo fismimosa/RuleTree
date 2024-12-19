9f41a2daaa8d794b05de40a6f1bf292f10da149f
acascione <117826770+acascione@users.noreply.github.com>
2024-12-19 16:53:28 +0100

    Update instance_stumps.py

--- RuleTree/stumps/instance_stumps.py
+++ RuleTree/stumps/instance_stumps.py
@@ -1,11 +1,12 @@
-from ruletree.stumps.classification.DecisionTreeStumpClassifier import DecisionTreeStumpClassifier
-from ruletree.stumps.classification.ObliqueDecisionTreeStumpClassifier import ObliqueDecisionTreeStumpClassifier
-from ruletree.stumps.classification.PivotTreeStumpClassifier import PivotTreeStumpClassifier
-from ruletree.stumps.classification.MultiplePivotTreeStumpClassifier import MultiplePivotTreeStumpClassifier
-#from ruletree.stumps.classification.ProximityTreeStumpClassifier import ProximityTreeStumpClassifier
+from RuleTree.stumps.classification.DecisionTreeStumpClassifier import DecisionTreeStumpClassifier
+from RuleTree.stumps.classification.ObliqueDecisionTreeStumpClassifier import ObliqueDecisionTreeStumpClassifier
+from RuleTree.stumps.classification.PivotTreeStumpClassifier import PivotTreeStumpClassifier
+from RuleTree.stumps.classification.MultiplePivotTreeStumpClassifier import MultiplePivotTreeStumpClassifier
+#from RuleTree.stumps.classification.ProximityTreeStumpClassifier import ProximityTreeStumpClassifier
 
+from RuleTree.stumps.classification.ObliquePivotTreeStumpClassifier import ObliquePivotTreeStumpClassifier
 
-from ruletree.stumps.regression.DecisionTreeStumpRegressor import DecisionTreeStumpRegressor
+from RuleTree.stumps.regression.DecisionTreeStumpRegressor import DecisionTreeStumpRegressor
 
 
 def dt_stump_reg_call(random_state = 42):
@@ -41,7 +42,13 @@ def dt_stump_call(random_state = 42):
     return dt_stump
 
 
-def obl_stump_call(random_state = 42):
+def obl_stump_call(random_state = 42, 
+                   oblique_split_type = 'euclidean',
+                   pca=None,
+                   max_oblique_features=2,
+                   tau=1e-4,
+                   n_orientations=10):
+    
     obl_stump = ObliqueDecisionTreeStumpClassifier(
                         max_depth=1,
                         criterion='gini',
@@ -55,8 +62,40 @@ def obl_stump_call(random_state = 42):
                         class_weight=None,
                         ccp_alpha=0.0,
                         monotonic_cst = None,
+                        oblique_split_type = oblique_split_type,
+                        pca = pca,
+                        max_oblique_features = max_oblique_features,
+                        tau = tau,
+                        n_orientations = n_orientations
                         )
     return obl_stump
+
+def obl_pt_stump_call(random_state = 42, 
+                   oblique_split_type = 'euclidean',
+                   pca=None,
+                   max_oblique_features=2,
+                   tau=1e-4,
+                   n_orientations=10):
+    obl_pt_stump = ObliquePivotTreeStumpClassifier(
+                        max_depth=1,
+                        criterion='gini',
+                        splitter='best',
+                        min_samples_split=2,
+                        min_samples_leaf = 1,
+                        min_weight_fraction_leaf=0.0,
+                        max_features=None,
+                        random_state=random_state,
+                        min_impurity_decrease=0.0,
+                        class_weight=None,
+                        ccp_alpha=0.0,
+                        monotonic_cst = None,
+                        oblique_split_type = oblique_split_type,
+                        pca = pca,
+                        max_oblique_features = max_oblique_features,
+                        tau = tau,
+                        n_orientations = n_orientations
+                        )
+    return obl_pt_stump
 
 
 def pt_stump_call(random_state = 42):
