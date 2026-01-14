b7a66b00acd178e0547a7d82c22443a0418501cb
cri98li <cri98li@gmail.com>
2026-01-14 11:34:49 +0100

    Update GBoostRegressor.py

--- RuleTree/ensemble/GBoostRegressor.py
+++ RuleTree/ensemble/GBoostRegressor.py
@@ -11,38 +11,56 @@ from RuleTree.base.RuleTreeBase import RuleTreeBase
 
 class GBoostRegressor(RuleTreeBase, RegressorMixin):
     def __init__(self, base_estimator=RuleTreeRegressor(max_depth=3),
-                 n_estimators=100, learning_rate=.1, loss='squared_loss', n_iter_no_change=None, tol=1e-4,):
+                 n_estimators=100, learning_rate=.1, loss='squared_loss', patience=5, val_size=.15,
+                 random_state=42):
         self.base_estimator = base_estimator
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         if loss != 'squared_loss' and type(loss) is str:
             raise ValueError('loss must be squared_loss or callable')
         self.loss = loss
-        self.n_iter_no_change = n_iter_no_change
-        self.tol = tol
+        self.patience = patience
+        self.random_state = random_state
+        self.val_size = val_size
 
     def _get_base_prediction(self, y):
         return np.mean(y)
 
     def fit(self, X: np.ndarray, y: np.ndarray):
-        self.base_prediction_ = self._get_base_prediction(y)
-        self.estimators_ = []
-        residuals = y - self.base_prediction_
+        np.random.seed(self.random_state)
+        n_val = int(self.val_size * X.shape[0])
+        idx = np.random.permutation(X.shape[0])
+        val_idx = idx[:n_val]
+        train_idx = idx[n_val:]
 
-        patience = self.n_iter_no_change
+        X_train, X_val = X[train_idx], X[val_idx]
+        y_train, y_val = y[train_idx], y[val_idx]
+
+        self.base_prediction_ = self._get_base_prediction(y_train)
+        self.estimators_ = []
+        residuals = y_train - self.base_prediction_
+        residuals_val = y_val - self.base_prediction_
+        prev_residuals_val = np.inf
+
+        wait = 0
         for _ in range(self.n_estimators):
             est = copy.deepcopy(self.base_estimator)
-            est.fit(X, residuals)
+            est.fit(X_train, residuals)
 
-            res_delta = self.learning_rate * est.predict(X)
-            if patience is not None:
-                if np.mean(np.abs(res_delta)) <= self.tol:
-                    patience -= 1
-                if patience == 0:
-                    self.estimators_ = self.estimators_[:-self.n_iter_no_change]
-                    break
+            res_delta = self.learning_rate * est.predict(X_train)
+            res_delta_val = self.learning_rate * est.predict(X_val)
             residuals -= res_delta
+            residuals_val -= res_delta_val
+
             self.estimators_.append(est)
+            if np.sum(np.abs(residuals_val)) <= prev_residuals_val:
+                wait = 0
+            else:
+                wait += 1
+                if wait >= self.patience:
+                    self.estimators_ = self.estimators_[:-self.patience+1]
+                    break
+            prev_residuals_val = np.sum(np.abs(residuals_val))
 
         return self
 
